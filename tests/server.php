db69a564cd027f02377401170bbf6c2dc3e5be95
Marc Serrat <mserrat@odin.com>
2018-10-11 16:47:45 +0200

    Fixed gitattributes, removed server.php test file and updated Readme

--- tests/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * This file is part of the Ingram Micro Cloud Blue Connect SDK.
- *
- * @copyright (c) 2018. Ingram Micro. All Rights Reserved.
- */
-
-print trim(file_get_contents(__DIR__ . '/request.json'));
