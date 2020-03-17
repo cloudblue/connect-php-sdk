c6aa4b6611775a5283c708e1d0591ca6319cf90a
Marc Serrat <Marc.SerratBote@ingrammicro.com>
2020-03-17 12:23:09 +0100

    Changed copyright to include date range

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the Ingram Micro Cloud Blue Connect SDK.
  *
- * @copyright (c) 2020. Ingram Micro. All Rights Reserved.
+ * @copyright (c) 2018-2020. Ingram Micro. All Rights Reserved.
  */
 
 namespace Test;
